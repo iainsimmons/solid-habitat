7191588fbdbd978e50ace4b689e2f4e08b248f18
Iain Simmons <isimmons@squiz.net>
2022-09-04 23:59:22 +1000

    Change package name of core

--- packages/examples/src/index.tsx
+++ packages/examples/src/index.tsx
@@ -1,5 +1,5 @@
 /* @refresh reload */
-import SolidHabitat from '@solid-habitat/core';
+import SolidHabitat from 'solid-habitat';
 import './index.css';
 
 const components = import.meta.glob('./components/**/*.[jt]sx', {
