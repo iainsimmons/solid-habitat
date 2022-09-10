69552e3d74bdf17db6e061ae966af246ec0d70b3
Iain Simmons <isimmons@squiz.net>
2022-09-11 01:30:52 +1000

    Update data attributes and fix dupes + allow named export/import + add data-habited to track loaded components

--- packages/core/index.tsx
+++ packages/core/index.tsx
@@ -6,16 +6,17 @@ import toKebabCase from './utils/toKebabCase';
 import toPascalCase from './utils/toPascalCase';
 
 export default function SolidHabitat(componentEntries) {
-  const roots = document.querySelectorAll('[data-component]');
+  const roots = document.querySelectorAll('[data-habitat]');
 
   roots.forEach((root) => {
-    const { component, ...otherProps } = root.dataset;
+    const { habited, habitat: component, ...otherProps } = root.dataset;
+
     const dataProps = Object.entries(otherProps ?? {}).reduce(
       (props, [key, value]) => {
-        if (key.startsWith('propN')) {
+        if (key.startsWith('nProp')) {
           props[toCamelCase(key.substring(5))] = parseFloat(value);
         } else if (
-          key.startsWith('propB') &&
+          key.startsWith('bProp') &&
           ['true', 'false'].includes(value)
         ) {
           props[toCamelCase(key.substring(5))] = JSON.parse(value);
@@ -34,12 +35,18 @@ export default function SolidHabitat(componentEntries) {
         path.includes(`/${component}/${toPascalCase(component)}.`) ||
         path.includes(`/${toKebabCase(component)}/${toKebabCase(component)}.`)
     )?.[1];
-    console.log({ foundComponent, type: typeof foundComponent });
+
+    if (!foundComponent) {
+      root.setAttribute('data-habited', 'false');
+      return;
+    }
 
     const Component =
       typeof foundComponent === 'function'
         ? foundComponent
-        : foundComponent?.default || null;
+        : foundComponent[toPascalCase(component)] ||
+          foundComponent?.default ||
+          null;
 
     let scriptProps;
     try {
@@ -49,9 +56,12 @@ export default function SolidHabitat(componentEntries) {
       scriptProps = {};
     }
 
-    render(
-      () => <Dynamic component={Component} {...dataProps} {...scriptProps} />,
-      root
-    );
+    if (!habited) {
+      render(
+        () => <Dynamic component={Component} {...dataProps} {...scriptProps} />,
+        root
+      );
+      root.setAttribute('data-habited', 'true');
+    }
   });
 }
